23e73c9782d2a3a2bea7acc08974eee94d678e31
Caio Granero <caio.granero@usp.br>
2018-08-21 22:14:58 -0300

    Faltou uma letra na exceção do middleware

--- src/utils/Middleware.js
+++ src/utils/Middleware.js
@@ -5,28 +5,32 @@ const authException = [{
   url: '/api/token',
   method: 'GET',
 }, {
-  url: '/api/usarios',
+  url: '/api/usuarios',
   method: 'POST',
 }];
 
 const Middleware = {
   Auth(req, res, next) {
-    const isException = authException.some(ex => req.url.includes(ex.url) && req.method === ex.method);
+    const isException = authException
+      .some(ex => req.url.toLowerCase().includes(ex.url.toLowerCase())
+        && req.method.toUpperCase() === ex.method.toUpperCase());
 
     if (!isException) {
       if (!req.headers.authorization) {
         res.status(400).send({ error: 'missing authorization header' });
       }
 
-      TokenController.Verify(req.headers.authorization, environment.application.application.secret).then((isAuthorizate) => {
-        if (!isAuthorizate) {
-          res.status(401).send({ error: 'Você está sem acesso.' });
-        }
+      TokenController
+        .Verify(req.headers.authorization, environment.application.application.secret)
+        .then((isAuthorizate) => {
+          if (!isAuthorizate) {
+            res.status(401).send({ error: 'Você está sem acesso.' });
+          }
 
-        next();
-      }).catch((err) => {
-        res.status(401).send({ error: 'Você está sem acesso.' });
-      });
+          next();
+        }).catch((err) => {
+          res.status(401).send({ error: 'Você está sem acesso.' });
+        });
     } else {
       next();
     }
