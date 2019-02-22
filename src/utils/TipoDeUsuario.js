e0a7af89fe5d1359ef0cac7027029d9ac1ca84da
Caio Granero <caio.granero@usp.br>
2019-02-22 20:21:38 -0300

    Removendo arquivos que não eram utilizados.

--- src/utils/TipoDeUsuario.js
+++
@@ -1,8 +0,0 @@
-const Enum = require('./Enum');
-
-const TipoDeUsuario = Object.freeze({
-  Personal: new Enum('Personal', 1),
-  Aluno: new Enum('Aluno', 2),
-});
-
-module.exports = TipoDeUsuario;
